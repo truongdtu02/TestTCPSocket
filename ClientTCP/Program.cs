f2e5f8f1eb6db2d878efcd29f64819760049f85b
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-03-28 16:47:34 +0700

    Add project files.

---
+++ ClientTCP/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Tcp Client";
+            IPAddress serverIP;
+            // yêu cầu người dùng chon server
+            Console.Write("Server: 1-local , 2-cloud");
+            var choiceServer = Console.ReadKey();
+            if(choiceServer.KeyChar == '1') {
+                serverIP = IPAddress.Parse("127.0.0.1");
+            }
+            else
+            {
+                serverIP = IPAddress.Parse("45.118.145.137"); 
+            }
+
+            int serverPort = 1308;
+
+            // đây là "địa chỉ" của tiến trình server trên mạng
+            // mỗi endpoint chứa ip của host và port của tiến trình
+            var serverEndpoint = new IPEndPoint(serverIP, serverPort);
+
+            var size = 16000; // kích thước của bộ đệm
+            var receiveBuffer = new byte[size]; // mảng byte làm bộ đệm            
+
+            Stopwatch watch = new Stopwatch();
+
+            // khởi tạo object của lớp socket để sử dụng dịch vụ Tcp
+            // lưu ý SocketType của Tcp là Stream 
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            double timepoint = 0, mark1, mark2;
+            int frame = 0, delay = 0;
+            while (true)
+            {
+                try
+                {
+                    // tạo kết nối tới Server
+                    socket.Connect(serverEndpoint);
+                    watch.Start();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine (ex);
+                    continue;
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        // nhận mảng byte từ dịch vụ Tcp và lưu vào bộ đệm
+                        mark1 = watch.ElapsedMilliseconds;
+                        var length = socket.Receive(receiveBuffer);
+                        mark2 = watch.ElapsedMilliseconds;
+                        frame++;
+                        timepoint = mark2 - mark1;
+                        if (timepoint > 1000)
+                        {
+                            delay++;
+                        }
+                        
+                        if (frame % 3 == 0)
+                        {
+                            Console.WriteLine("Frame: {0} , Delay: {1}, Time: {2}, Length: {3}", frame, delay, (int)timepoint, length);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ TestTCPSocket/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TestTCPSocket
+{
+    internal class Program
+    {
+        Thread receiveThread;
+        private static void Main(string[] args)
+        {
+            Console.Title = "Tcp Server";
+
+            // giá trị Any của IPAddress tương ứng với Ip của tất cả các giao diện mạng trên máy
+            var localIp = IPAddress.Any;
+            // tiến trình server sẽ sử dụng cổng tcp 1308
+            var localPort = 1308;
+            // biến này sẽ chứa "địa chỉ" của tiến trình server trên mạng
+            var localEndPoint = new IPEndPoint(localIp, localPort);
+
+            // tcp sử dụng đồng thời hai socket: 
+            // một socket để chờ nghe kết nối, một socket để gửi/nhận dữ liệu
+            // socket listener này chỉ làm nhiệm vụ chờ kết nối từ Client
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // yêu cầu hệ điều hành cho phép chiếm dụng cổng tcp 1308
+            // server sẽ nghe trên tất cả các mạng mà máy tính này kết nối tới
+            // chỉ cần gói tin tcp đến cổng 1308, tiến trình server sẽ nhận được
+            listener.Bind(localEndPoint);
+            // bắt đầu lắng nghe chờ các gói tin tcp đến cổng 1308
+            listener.Listen(10);
+            Console.WriteLine($"Local socket bind to {localEndPoint}. Waiting for request ...");
+
+            while (true)
+            {
+                // tcp đòi hỏi một socket thứ hai làm nhiệm vụ gửi/nhận dữ liệu
+                // socket này được tạo ra bởi lệnh Accept
+                Socket socket;
+                try
+                {
+                    socket = listener.Accept();
+                    if (socket.Connected)
+                    {
+                        Console.WriteLine($"Accepted connection from {socket.RemoteEndPoint}");
+                        Thread thread = new Thread(() =>
+                        {
+                            Listenning(socket);
+                        });
+                        thread.Start();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }          
+            }       
+            //// đóng kết nối và giải phóng tài nguyên
+            //Console.WriteLine($"Closing connection from {socket.RemoteEndPoint}rn");
+            //socket.Close();
+        }
+
+        private static void Listenning(Socket socket){
+            int size = 1024;
+            byte[] receiveBuffer = new byte[size];
+            socket.ReceiveTimeout = 3000;
+            int length;
+            while (true)
+            {         
+                try
+                {
+                    // nhận dữ liệu vào buffer
+                    length = socket.Receive(receiveBuffer);
+                    socket.SendTimeout = 500;
+                }
+                catch
+                {
+                    socket.Close();
+                    Console.WriteLine("Socket close!");
+                    break;
+                }                        
+                var text = Encoding.ASCII.GetString(receiveBuffer, 0, length);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
